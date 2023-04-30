78e61335670275fbc6446165c034789576dd9094
Aidan Chao <achao@udel.edu>
2023-04-30 14:04:53 -0400

    saved scholarships page now pulls from firebase, also minor UI updates

--- scholar-match/src/components/GridCard.js
+++ scholar-match/src/components/GridCard.js
@@ -5,13 +5,13 @@ import scholarshipLogo from '../images/gradcap.png'
 
 const { Meta } = Card;
 
-const GridCard = () => {
+const GridCard = ({scholarshipName, scholarshipDescription, scholarshipAmount, scholarshipDeadline, scholarshipURL}) => {
     return (
         <div class="mt-2">
             <Col>
                 <Card hoverable style={{ width: 330 }} cover={<img src={scholarshipLogo} alt="pic of grad cap"></img>}>
-                    <Meta title="Sample Saved Scholarship" description="More info will be put about each scholarship here" />
-                        <a class="btn btn-primary mt-2" href="#" role="button">Apply</a>
+                    <Meta title={scholarshipName} description={scholarshipDescription} />
+                        <a class="btn btn-primary mt-2" href={scholarshipURL} role="button">Apply</a>
                 </Card>
             </Col>
         </div>
--- scholar-match/src/pages/savedscholarships.js
+++ scholar-match/src/pages/savedscholarships.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Row } from "antd";
 import GridCard from "../components/GridCard"
-  
+import db from '../firebase.config'
+
 const SavedScholarships = () => {
+
+  const [savedScholarships, setSavedScholarships] = useState([]);
+
+  useEffect(() => {
+    Fetchdata();
+  }, [])
+
+  const Fetchdata = async () => {
+    db.collection("SavedScholarships").get().then((querySnapshot) => {
+      querySnapshot.forEach(element => {
+        var data = element.data();
+        setSavedScholarships(arr => [...arr, data])
+      })
+    })
+  }
+
   return (
     <div>
-      <Row gutter={[20,20]}>
-        <GridCard/>
-        <GridCard/>
-        <GridCard/>
-        <GridCard/>
-        <GridCard/>
+      <Row gutter={[20, 20]}>
+        {
+          savedScholarships.map((savedScholarship) => (
+            <GridCard scholarshipName={savedScholarship.Name} scholarshipDescription={savedScholarship.Description} scholarshipAmount={savedScholarship.Amount} scholarshipDeadline={savedScholarship.Deadline.toDate().toDateString()} scholarshipURL={savedScholarship.URL}/>
+          ))
+        }
       </Row>
     </div>
   );
 };
-  
+
 export default SavedScholarships;
