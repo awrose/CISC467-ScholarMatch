d171791dcc7c82f5f4f41f6bba527e30a8fb909d
awrose <awrose@udel.edu>
2023-05-15 19:23:15 -0400

    added modal to main page

--- scholar-match/src/components/HomePage/Card.js
+++ scholar-match/src/components/HomePage/Card.js
@@ -1,37 +1,27 @@
-import { Card, Col } from 'antd'
+import { Card, Col, Modal } from 'antd'
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import scholarshipLogo from '../../images/gradcap.png'
-import {useNavigate} from "react-router-dom";
 import Button from 'react-bootstrap/Button'
 import { useState } from 'react';
 
-//const { Meta } = Card;
-
 const ScholarshipCard = ({scholarship, scholarships, setScholarships}) => {
 
-    //const [showModal, setShowModal] = useState(false);
     const [disabled, setDisabled] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
-    //let navigate = useNavigate();
     const saveScholarship = () =>{
-        //scholarship.Saved = true;
-        //check if the scholarship is already in savedscholarships
         setScholarships(scholarships.map(oldScholarship => oldScholarship.Id === scholarship.Id ? {...oldScholarship, Saved: true} : {...oldScholarship}))
         alert("Scholarship Successfully Saved")
         setDisabled(true)
-        
-        console.log(scholarships)
-
     }
 
-    const handleCardClick = () =>{
-        //do something
-        //setShowModal(showModal => !showModal);
+    const changeShowModal = () =>{
+        setShowModal(!showModal)
     }
 
     return (
-        <div onClick={handleCardClick} class="mt-2">
+        <div onClick={changeShowModal} class="mt-2">
             <Col>
                 <Card hoverable style={{ width: 330 }} cover={<img src={scholarshipLogo} alt="pic of grad cap"></img>}>
                     <h3>{scholarship.Name}</h3>
@@ -43,9 +33,26 @@ const ScholarshipCard = ({scholarship, scholarships, setScholarships}) => {
                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         &nbsp;&nbsp;&nbsp;
-                    <Button disabled = {disabled} color = "primary" onClick={saveScholarship}>Save</Button>
+                    <Button disabled = {disabled} variant = "primary" onClick={saveScholarship}>Save</Button>
                 </Card>
             </Col>
+            <Modal 
+                open={showModal} 
+                footer={[
+                    <a class = "btn btn-primary mt-2" href={scholarship.URL} role="button">Apply</a>,
+                    <Button disabled = {disabled} variant = "primary" onClick={saveScholarship}>Save</Button>
+                ]}>
+                    <h3 style={{textAlign: 'center'}}>{scholarship.Name}</h3>
+                    <div style={{display: 'flex', justifyContent:'space-between'}}>
+                        <h5>${scholarship.Amount}</h5>
+                        <h5><b>Deadline: </b>{scholarship.Deadline}</h5>
+                    </div>
+                    <h6>Amount of Awards Offered: {scholarship.Awards}</h6>
+                    <h5>Eligibility: </h5>
+                    <p>{scholarship.Eligibility}</p>
+                    <h5>Description: </h5>
+                    <p>{scholarship.Description}</p>
+            </Modal>
         </div>
     );
 };
