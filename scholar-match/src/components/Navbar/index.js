98d80abb7776b831f54d5dcad70f34af1b921e15
awrose <awrose@udel.edu>
2023-10-19 21:12:28 -0400

    trying to fix navbar

--- scholar-match/src/components/Navbar/index.js
+++ scholar-match/src/components/Navbar/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Nav, NavLink, NavMenu } 
     from "./NavbarElements";
 import "./navbar.css";
+import Home from "../../pages/index.js";
+import Login from "../../pages/login";
+import SavedScholarships from "../../pages/savedscholarships";
   
 const Navbar = () => {
   return (
@@ -9,15 +12,15 @@ const Navbar = () => {
       <Nav>
         <h1>Scholar Match</h1>
         <NavMenu>
-          <NavLink to="/" activeStyle>
+          <NavLink to="/" activeStyle exact component={Home}>
             Home
           </NavLink>
-          <NavLink to="/login" activeStyle>
+          <NavLink to="/login" activeStyle exact component={Login}>
             Log in/Sign up
           </NavLink>
-          <NavLink to="/savedscholarships" activeStyle>
+          <NavLink to="/savedscholarships" activeStyle exact component={SavedScholarships}>
             Saved Scholarships
-          </NavLink>
+          </NavLink> 
         </NavMenu>
       </Nav>
     </>
